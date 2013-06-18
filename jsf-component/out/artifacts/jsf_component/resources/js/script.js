63fd9e2c6f0e106858702d96989b185d691ce9f4
Rafael Pereira <rafaelsouza@sistemafieg.org.br>
2013-06-18 14:31:23 -0300

    Removendo out

--- jsf-component/out/artifacts/jsf_component/resources/js/script.js
+++
@@ -1,8 +0,0 @@
-var inputs = document.getElementsByTagName("div");
-for(i=0;i<inputs.length;i++){
-    try{
-        inputs[i].onmouseover = function(){console.log("teste");}
-    }catch(e){
-
-    }
-}
