73947d37e68738bfc7697945c126830d87093005
Rafael Pereira <rafael.hardrock@gmail.com>
2013-10-22 18:24:03 -0200

    "Agora sim um layout bonito"

--- angularjs-layout/src/main/webapp/js/index.js
+++ angularjs-layout/src/main/webapp/js/index.js
@@ -2,6 +2,10 @@ angular.module('bootstrap', ['ui.bootstrap']);
 
 function IndexCTRL($scope){
     
-    
+    $scope.projeto = {
+        "nome":  "AngularJS-Layout",
+        "versao": "0.1-ALPHA"
+        
+    }
 }
 
