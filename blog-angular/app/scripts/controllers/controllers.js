40c57d9712d9d567a523ad58c48ea60dc29ad0f3
Rafael Pereira <rafael@getin-rafael.sistemafieg.net>
2014-04-25 16:09:05 -0300

    blog angular

---
+++ blog-angular/app/scripts/controllers/controllers.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var blogAngularControllers = angular.module('blogAngularControllers',[]);
+
+blogAngularControllers.controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma','Rafael'
+    ];
+  });
+
+  
